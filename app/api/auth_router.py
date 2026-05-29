79abe4fd99dbcb541f31740544d2a89ba5954c77
kridayslifestyle <kridayslifestyle@gmail.com>
2026-05-29 18:00:34 +0530

    reuired pages created

--- app/api/auth_router.py
+++ app/api/auth_router.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from fastapi.responses import RedirectResponse
+import requests
+import os
 
 from app.services.meta_service import get_meta_login_url
 
@@ -11,7 +13,27 @@ router = APIRouter(
 
 @router.get("/login")
 def meta_login():
-
     return RedirectResponse(
         get_meta_login_url()
     )
+
+
+@router.get("/callback")
+def meta_callback(code: str = Query(...)):
+
+    token_response = requests.get(
+        "https://graph.facebook.com/v23.0/oauth/access_token",
+        params={
+            "client_id": os.getenv("META_APP_ID"),
+            "client_secret": os.getenv("META_APP_SECRET"),
+            "redirect_uri": os.getenv("META_REDIRECT_URI"),
+            "code": code,
+        },
+    )
+
+    token_data = token_response.json()
+
+    return {
+        "success": True,
+        "token_data": token_data
+    }
