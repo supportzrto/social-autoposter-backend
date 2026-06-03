4f8850df243808f480501eb8314fd2ac92a5764e
supportzrto <support.zrto@gmail.com>
2026-06-03 18:18:48 +0530

    main.py corrected

--- app/api/auth_router.py
+++ app/api/auth_router.py
@@ -10,6 +10,7 @@ from app.models.brand_model import Brand
 from app.dependencies.auth_dependency import get_current_user
 from app.models.user_model import User
 from fastapi import Depends
+from fastapi import HTTPException
 
 router = APIRouter(
     prefix="/auth/meta",
@@ -132,3 +133,36 @@ def meta_callback(
         "instagram_business_id": brand.instagram_business_id,
     }
 
+@router.get("/pages")
+def get_pages(
+    db: Session = Depends(get_db),
+    current_user: User = Depends(
+        get_current_user
+    )
+):
+
+    brand = (
+        db.query(Brand)
+        .filter(
+            Brand.user_id ==
+            current_user.id
+        )
+        .first()
+    )
+
+    if not brand:
+
+        raise HTTPException(
+            status_code=404,
+            detail="No connected brand found"
+        )
+
+    response = requests.get(
+        "https://graph.facebook.com/v23.0/me/accounts",
+        params={
+            "access_token":
+                brand.access_token
+        }
+    )
+
+    return response.json()
